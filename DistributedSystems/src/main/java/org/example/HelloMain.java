a3cee13e0643860bf37ee3b597d0c95f8a58013c
Helon21 <helonbentes51@hotmail.com>
2024-02-28 20:28:22 +0000

    Starting Repository

---
+++ DistributedSystems/src/main/java/org/example/HelloMain.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.classes.HelloWorld;
+
+public class HelloMain {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+
+        helloWorld.setName("Helon Musk");
+        helloWorld.sayHello();
+
+    }
+}
